2b4ab083e0312ab3c1705d8c3ad7d2ac0a16b4b3
sit79 <stefan.tolksdorf@gmail.com>
2020-08-20 20:22:38 +0200

    TODO added
    split files in found and not found books

--- app/save.js
+++ app/save.js
@@ -6,6 +6,9 @@ const save = async (readableResult) => {
   // Save to file:
   await csv.toDisk("./result/books.csv");
 
+  //TODO save two files: file1 contains finds,
+  // file2 contains books that could not be found
+
   // Return the result as table of strings if needed
   console.table(await readableResult);
 };
