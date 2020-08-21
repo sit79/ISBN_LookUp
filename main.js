723eb15545e50422da3f7e21d4e7f020a715704c
sit79 <stefan.tolksdorf@gmail.com>
2020-08-21 17:55:13 +0200

    terminal spinner added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// const isbnCollection = require("./app/data");
+const ora = require("ora");
 const readInput = require("./app/readInput");
 const findMyBooks = require("./app/findMyBooks");
 const createResult = require("./app/createResult");
@@ -6,10 +6,16 @@ const report = require("./app/report");
 const save = require("./app/save");
 
 const runWorkflow = async () => {
+  let spinner = ora("Reading input file").start();
   const path = "./input/input.csv";
   const isbnCollection = await readInput(path);
+  spinner.succeed();
+  spinner = ora("Calling openlibrary.org").start();
   const bookCollection = await findMyBooks(isbnCollection);
+  spinner.succeed();
+  spinner = ora("Creating output files").start();
   const readableResult = await createResult(bookCollection);
+  spinner.succeed().stop();
   await save(readableResult);
   await report(readableResult);
 };
