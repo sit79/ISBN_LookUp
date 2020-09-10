e7ec9174f273866799c4b46eb41f68a07384a9d9
sit79 <stefan.tolksdorf@gmail.com>
2020-09-10 16:35:32 +0200

    length property does not have to be evaluated twice

--- app/report.js
+++ app/report.js
@@ -2,9 +2,7 @@ const report = (readableResult) => {
   const searched = readableResult.length;
   const found = readableResult.filter((book) => book.found === true).length;
   const notFound = searched - found;
-  console.log(
-    `${notFound} of ${readableResult.length} books could not be found.`
-  );
+  console.log(`${notFound} of ${searched} books could not be found.`);
 };
 
 module.exports = report;
